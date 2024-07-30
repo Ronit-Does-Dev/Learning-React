a9697c8d3a4c5fb5f249a9a4c1fd2963ebe9c42c
Ronit Does Dev <ronit.rajput.dev@gmail.com>
2024-07-30 18:35:59 +0530

    header update

--- concurr/README.md
+++ concurr/README.md
@@ -7,8 +7,9 @@
 <li>Learn Async Programming and fetching data from URLs</li>
 <li>Form Handling</li>
 <li>React Router</li>
-<li>Intergrate Lottie Animations</li>
+<li>Integrate Lottie Animations</li>
 <li>Using Framer Motion for animation</li>
 <li>Custom Cursor</li>
 <li>Svg Animations</li>
+<li>sfx in react app</li>
 </ol>
--- concurr/src/components/NavBar.jsx
+++ concurr/src/components/NavBar.jsx
@@ -1,23 +1,27 @@
 
 function NavBar() {
   return (
-    <nav className="bg-gray-800 p-4 flex justify-between items-center">
+    <nav className="bg-[#EAF4CF] h-[94px] w-[100vw] flex justify-stretch items-center border-b-2 border-[#D5E0B6] leading-[100%]">
       {/* Logo */}
-      <div className="text-white text-[22px]">
-        <a href="/">Logo</a>
+      <div className="font-bebas text-black text-[22px] w-[20%] text-center hover:text-[1.5em]">
+        <a href="/">CONCURR</a>
       </div>
       
       {/* Navigation Links */}
-      <div className="flex space-x-6 text-white text-[22px]">
+      <div className="font-sans text-lg flex items-center justify-center gap-5 text-black text-[1.1rem] grow">
         <a href="/exchange" className="hover:text-gray-400">Exchange</a>
         <a href="/about" className="hover:text-gray-400">About</a>
         <a href="/pricing" className="hover:text-gray-400">Pricing</a>
         <a href="/history" className="hover:text-gray-400">History</a>
       </div>
+
+      <div className="h-[100%] w-[20%] hidden min-[850px]:flex justify-center items-center text-center text-black text-[22px] font-regular">
+        <a href="tel:+91-780-185-9576">+91 7801859576</a>
+      </div>
       
       {/* Log In Button */}
-      <div>
-        <a href="/login" className="text-white bg-blue-500 hover:bg-blue-700 text-[22px] font-bold py-2 px-4 rounded">
+      <div className="h-[100%] w-[20%] flex justify-center items-center text-center text-black bg-[#FAFFE6] hover:bg-[#f2ffbd] text-[22px] font-regular">
+        <a href="/login">
           Log In
         </a>
       </div>
